d100bfa9e3a5745f4fa05ceda43a32efff6f0ded
Emil Mattsson <emilmattsson14@gmail.com>
2023-01-27 14:06:10 +0100

    crypto project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typograhphy, Space } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = ({ Title }) => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">We are in navbar</Link>
+        </Typography.Title>
+        <Button className="menu-control-container"></Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
