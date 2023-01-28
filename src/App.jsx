d35024ee928d11e7cc3f5f78e9f64b87464f3cdc
Emil Mattsson <emilmattsson14@gmail.com>
2023-01-28 23:20:08 +0100

    fixing bug in main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.scss";
-import { Switch, Route, Link } from "react-router-dom";
-import { Layout, Typograhphy, Space } from "antd";
+// import { Switch, Route, Link } from "react-router-dom";
+// import { Layout, Typograhphy, Space } from "antd";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <div className="navbar"></div>
+      dhksadhkajshdjkashdkjlashdkljhsakljdhsdjkhfskjdhfkjsladhfkjsahdfkjhsd
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
+
 import {
   HomeOutlined,
   MoneyCollectOutlined,
@@ -8,16 +9,17 @@ import {
   FundOutlined,
   MenuOutlined,
 } from "@ant-design/icons";
+import icon from "../Images/cryptocurrency.png";
 
 const Navbar = ({ Title }) => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar />
+        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">We are in navbar</Link>
         </Typography.Title>
-        <Button className="menu-control-container"></Button>
+        {/* <Button className="menu-control-container"></Button> */}
       </div>
     </div>
   );
