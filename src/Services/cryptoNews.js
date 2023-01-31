36b81623878d853b5a8209b6f933b08db6dcebfa
Emil Mattsson <emilmattsson14@gmail.com>
2023-02-01 00:53:20 +0100

    news rendered on page

--- src/Services/cryptoNews.js
+++ src/Services/cryptoNews.js
@@ -1,38 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
-
-// const API_NEWS_KEY = import.meta.env.VITE_API_NEWS_KEY;
-// const API_NEWS_HOST = import.meta.env.VITE_API_NEWS_HOST;
-
-// const cryptoNewsHeaders = {
-//   "X-BingApis-SDK": "true",
-//   "X-RapidAPI-Key": API_NEWS_KEY,
-//   "X-RapidAPI-Host": API_NEWS_HOST,
-// };
-
-// const baseUrl = "https://bing-news-search1.p.rapidapi.com";
-
-// const createRequest = (url) => ({
-//   method: "GET",
-//   url: `${baseUrl}${url}`,
-//   params: { safeSearch: "Off", textFormat: "Raw" },
-//   headers: cryptoNewsHeaders,
-// });
-
-// export const cryptoNews = createApi({
-//   reducerPath: "cryptoNews",
-//   baseQuery: fetchBaseQuery({ baseUrl }),
-//   endpoints: (builder) => ({
-//     getCryptoNews: builder.query({
-//       query: ({ newsCategory, count }) =>
-//         createRequest(
-//           `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
-//         ),
-//     }),
-//   }),
-// });
-
-// export const { useGetCryptoNewsQuery } = cryptoNews;
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
 let API_NEWS_KEY;
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -12,10 +12,26 @@ import React from "react";
 export default function News({ simplified }) {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrnecy",
-    count: simplified ? 10 : 100,
+    count: simplified ? 1 : 12,
   });
 
-  console.log(cryptoNews);
+  if (!cryptoNews?.value) return "Loading...";
 
-  return <div>News</div>;
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
