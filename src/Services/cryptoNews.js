d2cc1c7cea671f603fb50099b243189cf3e9c508
Emil Mattsson <emilmattsson14@gmail.com>
2023-01-31 19:17:51 +0100

    init

--- src/Services/cryptoNews.js
+++ src/Services/cryptoNews.js
@@ -1,7 +1,49 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+// const API_NEWS_KEY = import.meta.env.VITE_API_NEWS_KEY;
+// const API_NEWS_HOST = import.meta.env.VITE_API_NEWS_HOST;
+
+// const cryptoNewsHeaders = {
+//   "X-BingApis-SDK": "true",
+//   "X-RapidAPI-Key": API_NEWS_KEY,
+//   "X-RapidAPI-Host": API_NEWS_HOST,
+// };
+
+// const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+// const createRequest = (url) => ({
+//   method: "GET",
+//   url: `${baseUrl}${url}`,
+//   params: { safeSearch: "Off", textFormat: "Raw" },
+//   headers: cryptoNewsHeaders,
+// });
+
+// export const cryptoNews = createApi({
+//   reducerPath: "cryptoNews",
+//   baseQuery: fetchBaseQuery({ baseUrl }),
+//   endpoints: (builder) => ({
+//     getCryptoNews: builder.query({
+//       query: ({ newsCategory, count }) =>
+//         createRequest(
+//           `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+//         ),
+//     }),
+//   }),
+// });
+
+// export const { useGetCryptoNewsQuery } = cryptoNews;
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
-const API_NEWS_KEY = import.meta.env.VITE_API_NEWS_KEY;
-const API_NEWS_HOST = import.meta.env.VITE_API_NEWS_HOST;
+let API_NEWS_KEY;
+let API_NEWS_HOST;
+
+try {
+  API_NEWS_KEY = import.meta.env.VITE_API_NEWS_KEY;
+  API_NEWS_HOST = import.meta.env.VITE_API_NEWS_HOST;
+} catch (error) {
+  console.error("Error: Failed to get API_NEWS_KEY and API_NEWS_HOST");
+}
 
 const cryptoNewsHeaders = {
   "X-BingApis-SDK": "true",
@@ -25,7 +67,8 @@ export const cryptoNews = createApi({
     getCryptoNews: builder.query({
       query: ({ newsCategory, count }) =>
         createRequest(
-          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+          // `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+          `/news`
         ),
     }),
   }),
