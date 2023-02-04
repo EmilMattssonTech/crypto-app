56bc15c5365ed6ca08899554cc56ff025b78eb5a
Emil Mattsson <emilmattsson14@gmail.com>
2023-02-04 22:58:41 +0100

    exchanges changes

--- src/components/Exchanges/Exchanges.jsx
+++ src/components/Exchanges/Exchanges.jsx
@@ -1,7 +1,28 @@
-import "./Exchanges.scss";
-
 import React from "react";
+import millify from "millify";
+import { Collapse, Row, Col, Typography, Avatar } from "antd";
+import HTMLReactParser from "html-react-parser";
+
+import { useGetExchangesQuery } from "../services/cryptoApi";
+import Loader from "./Loader";
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 export default function Exchanges() {
-  return <div>Exchanges</div>;
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+      <Row></Row>
+    </>
+  );
 }
